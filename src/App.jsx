18a0f2b4288deb09fef7d598716846410ebdaedc
pmayur <thisispatil@gmail.com>
2020-11-04 18:06:38 +0530

    Adds control panel to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import ControlPanel from "./ControlPanel/ControlPanel";
 
 function App() {
     return (
         <div className="App">
             <div id="timer">
-              
+                <div>
+                    <ControlPanel />
+                </div>
+                <div></div>
             </div>
         </div>
     );
