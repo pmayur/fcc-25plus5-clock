36bab0988df0aead4fd0c2cf86a51877992c1f82
pmayur <thisispatil@gmail.com>
2020-11-04 18:06:04 +0530

    Introduces view to control duration

---
+++ src/ControlPanel/DurationControl/DurationControl.jsx
@@ -0,0 +1,21 @@
+import "./DurationControl.css";
+
+import addIcon from '../assets/baseline_add_circle_black_18dp.png';
+import removeIcon from '../assets/baseline_remove_circle_black_18dp.png'
+
+export default function DurationControl() {
+    return (
+        <div id="duration">
+            <div id="label">Break</div>
+            <div id="length">12</div>
+            <div id="control">
+                <div id="increment">
+                    <img src={addIcon} alt="alternatetext"/>
+                </div>
+                <div id="decrement">
+                    <img src={removeIcon} alt="alternatetext"/>
+                </div>
+            </div>
+        </div>
+    );
+}
