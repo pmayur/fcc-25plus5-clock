5f8e0012706de020dd6fb7102315d0c66ac455ee
pmayur <thisispatil@gmail.com>
2020-11-05 18:31:42 +0530

    Displays timer length in timer panel

--- src/TimerPanel/TimerPanel.jsx
+++ src/TimerPanel/TimerPanel.jsx
@@ -3,11 +3,25 @@ import "./TimerPanel.css";
 import startButton from "../assets/baseline_play_circle_filled_black_18dp.png";
 import removeButton from "../assets/baseline_remove_circle_black_18dp.png";
 
+import { useSelector } from "react-redux";
+
 export default function TimerPanel() {
+    const state = useSelector((state) => state);
+    const timeLeft = state.timeLeft;
+
     return (
         <div id="timer-panel">
             <div id="timer-label">Session</div>
-            <div id="time-left">29:20</div>
+            <div id="time-left">
+                {timeLeft.minutes < 10
+                    ? "0" + timeLeft.minutes
+                    : timeLeft.minutes}
+
+                :
+                {timeLeft.seconds < 10
+                    ? "0" + timeLeft.seconds
+                    : timeLeft.seconds}
+            </div>
             <div id="buttons">
                 <div id="start_stop">
                     <img
