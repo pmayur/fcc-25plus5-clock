8be26dda2f1b3f0d49a44e058a9c256094733750
pmayur <thisispatil@gmail.com>
2020-11-06 17:49:59 +0530

    Implements Reset functionality

--- src/TimerPanel/TimerPanel.jsx
+++ src/TimerPanel/TimerPanel.jsx
@@ -14,15 +14,13 @@ import {
     changeSeconds,
     toggleCurrentTimer,
     startStopTimer,
+    resetTimer
 } from "../actions";
 
 export default function TimerPanel() {
     const dispatch = useDispatch();
     const state = useSelector((state) => state);
-
-    console.log(state);
     const [time, setTime] = useState(state.sessionLength * 60);
-
     const [isStarted, setIsStarted] = useState(false);
 
     function useInterval(callback, delay) {
@@ -46,18 +44,24 @@ export default function TimerPanel() {
     }
 
     const startedActions = () => {
-        setTime(time - 1);
-        dispatch(changeMinutes(Math.floor(time / 60)));
-        dispatch(changeSeconds(time % 60));
+        console.log(time)
+        if (time >= 0) {
+            if(time === 0){
+                document.getElementById("beep").play();
+            }
 
-        if (time <= 0) {
-            if( state.currentTimer === TIMER_IN.SESSION) {
+            setTime(time - 1);
+            dispatch(changeMinutes(Math.floor(time / 60)));
+            dispatch(changeSeconds(time % 60));
+
+            
+        } else if (time < 0) {
+            if (state.currentTimer === TIMER_IN.SESSION) {
                 dispatch(changeMinutes(state.breakLength));
-                setTime(state.breakLength * 60)
             } else {
                 dispatch(changeMinutes(state.sessionLength));
-                setTime(state.sessionLength * 60)
             }
+            setTime(state.breakLength * 60 - 1);
             dispatch(changeSeconds(0));
             dispatch(toggleCurrentTimer());
         }
@@ -70,9 +74,9 @@ export default function TimerPanel() {
     }, 1000);
 
     const startTimer = () => {
-        setTime(10
-            // (state.timeLeft.minutes * 60 + state.timeLeft.seconds) - 1
-        )
+        setTime(
+            (state.timeLeft.minutes * 60 + state.timeLeft.seconds) - 1
+        );
         dispatch(startStopTimer());
         setIsStarted(true);
     };
@@ -81,11 +85,20 @@ export default function TimerPanel() {
         setIsStarted(false);
     };
 
+    const reset = () => {
+        setIsStarted(false);
+        document.getElementById("beep").pause();
+        document.getElementById("beep").currentTime = 0;
+        dispatch(resetTimer())
+    }
+
     return (
         <div id="timer-panel">
-            <div id="timer-label">
-                {state.currentTimer}
-            </div>
+            <audio
+                src="http://codeskulptor-demos.commondatastorage.googleapis.com/descent/gotitem.mp3"
+                id="beep"
+            />
+            <div id="timer-label">{state.currentTimer}</div>
             <div id="time-left">
                 {state.timeLeft.minutes < 10
                     ? "0" + state.timeLeft.minutes
@@ -96,19 +109,20 @@ export default function TimerPanel() {
                     : state.timeLeft.seconds}
             </div>
             <div id="buttons">
-                <div id="start_stop">
+                <div id="start_stop-section">
                     <img
                         src={isStarted ? pauseButton : startButton}
+                        id="start_stop"
                         alt="alternatetext"
-                        id="start-icon"
                         onClick={isStarted ? pauseTimer : startTimer}
                     />
                 </div>
-                <div id="reset">
+                <div id="reset-section">
                     <img
                         src={resetButton}
                         alt="alternatetext"
-                        id="remove-icon"
+                        id="reset"
+                        onClick={reset}
                     />
                 </div>
             </div>
